a467a4b4137c8d685845406616a626fe3136fd9c
unknown <alex.darmos@gmail.com>
2019-02-18 21:54:57 -0500

    fixed prompt placement issue

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 //function startGame- decided to play game and sets player name
 const startGame = () => {
     //prompt user if they would like to play game and say yes
@@ -16,7 +17,7 @@ const startGame = () => {
         console.log("What are you scared?");
     }
 
-}
+};
 
 //runs the game
 const startCombat = (playerName, playerHp, grantHp, wins) => {
@@ -32,44 +33,52 @@ const startCombat = (playerName, playerHp, grantHp, wins) => {
             if (playerHp > 0 && grantHp > 0) {
                 let damage = getRandomInt();
                 let damage1 = getRandomInt();
+                continueFight = prompt("Attack or Quit?");
                 playerHp = playerHp - damage;
                 grantHp = grantHp - damage1;
                 console.log(`${playerName} hp: ${playerHp}`);
-                console.log(`Grant hp: ${grantHp}`);
-                continueFight = prompt("Attack or Quit?");
+                console.log(`Grant hp: ${grantHp}`); 
             }
+            
             //adds a win when grant is defeated, resets grants hp
             else if (grantHp <= 0 && playerHp > 0) {
                 wins += 1;
                 if (wins < 3) {
                     grantHp = 10;
-                    console.log(`Win: ${wins}/3`)
+                    console.log(`Win: ${wins}/3`);
                 }
                 //game 0ver when wins equal 3
                 else if (wins === 3) {
                     console.log(`Congrats ${playerName}, you win.`);
-                    gameOver = true;
-                    continueFight = "Quit";
+                    // gameOver = true;
+                    // continueFight = "Quit";
+                    break;
                 }
                 //game over when player hp reaches zero first
             } else if (playerHp <= 0 && grantHp > 0) {
                 console.log(`${playerName} has ${playerHp} health, and Grant has ${grantHp} health, you lose.`);
-                gameOver = true;
-                continueFight = "Quit";
-            }
+                // gameOver = true;
+                // continueFight = "Quit";
+                break;
+                //when user hp and grantHp hp hit 0 or less, user loses
+            }   else if (playerHp <= 0 && grantHp <= 0 && wins < 3) {
+                console.log(`${playerName} put up a good fight.. but still loses!`);
+                break;
+            }   
         }   else if (continueFight === "Quit")  {
             gameOver = true;
             console.log("Quitters never win!")
+            break;
         }
 
     }
-}
+};
 
 //decideds damage dealt between 1-5
 const getRandomInt = () => {
     let calcDamage = Math.floor((Math.random() * 5) + 1);
     return calcDamage;
-}
+};
 startGame();
 
 
